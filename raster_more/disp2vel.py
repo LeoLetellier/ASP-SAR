ce8c2ed70b3d9550ba5c77af378f45d4bf1aa34d
Leo Letellier <leo.letellier.6@gmail.com>
2025-06-19 17:32:42 +0200

    Fix disp2vel and remove median

---
+++ raster_more/disp2vel.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+disp2vel.py
+--------------
+convert displacement to velocity
+
+Usage: disp2vel.py <raster> <output> [--res=<res>] --time=<time> [--band=<band>] [--flip]
+disp2vel.py -h | --help
+
+Options:
+-h | --help         Show this screen
+<raster>            Displacement raster
+<output>            Velocity output raster
+--res               Resolution in m
+--time              Time between acquisition in year
+--band              Band number
+--flip              Flip the velocity sign (for radar acquisition D -> flip horz, V -> flip Vert)
+
+"""
+
+import docopt
+import numpy as np
+from osgeo import gdal
+
+
+def open_raster(path, band=1, crop=None):
+    ds = gdal.Open(path)
+    band = ds.GetRasterBand(band)
+    ndv = band.GetNoDataValue()
+    if crop is None:
+        crop = [0, 0, band.XSize, band.YSize]
+    array = band.ReadAsArray(crop[0], crop[1], crop[2], crop[3])
+    if ndv is not None and ndv != np.nan:
+        array[array == ndv] = np.nan
+    return array
+
+
+def copy_raster(raster, output, band=1):
+    ds_raster = gdal.Open(raster)
+    driver = gdal.GetDriverByName('GTiff')
+    driver.CreateCopy(output, ds_raster, band)
+
+
+def disp2vel(output, res, time, flip=False):
+    ds = gdal.OpenEx(output, gdal.OF_VERBOSE_ERROR|gdal.OF_UPDATE)
+    band = ds.GetRasterBand(1)
+    ndv = band.GetNoDataValue()
+
+    for y in range(band.YSize):
+        line = open_raster(output, crop=[0, y, band.XSize, 1])
+        new_line = line * res / time
+        if flip:
+            new_line = -new_line
+        new_line[new_line == np.nan] = ndv
+        band.WriteArray(new_line, 0, y)
+
+
+if __name__ == "__main__":
+    arguments = docopt.docopt(__doc__)
+    raster = arguments["<raster>"]
+    output = arguments["<output>"]
+    res = arguments["--res"]
+    time = arguments["--time"]
+    band = arguments["--band"]
+    flip = arguments["--flip"]
+    if res is not None:
+        res = float(res)
+    time = float(time)
+    if band is not None:
+        band = int(band)
+    else:
+        band=1
+
+    print("copy raster...")
+    copy_raster(raster, output, band=band)
+
+    print("convert to velocity...")
+    disp2vel(output, res, time, flip=flip)
+
--- raster_more/remove_median.py
+++ raster_more/remove_median.py
@@ -5,20 +5,21 @@ remove_median.py
 --------------
 Prepare the directory structure for further processing. Link all ALL2GIF results in the given destination dir.
 
-Usage: remove_median.py <raster> [--crop=<crop>] <output> [--band=<band>]
+Usage: remove_median.py <raster> [--crop=<crop>] <output> [--band=<band>] [--plot]
 remove_median.py -h | --help
 
 Options:
 -h | --help         Show this screen
-<raster>
-<output>
-<crop>
-<band>
+<raster>            Raster to correct
+<output>            Corrected output raster
+<crop>              Area for median estimation
+<band>              Band number
 
 """
 
 import docopt
 import numpy as np
+import matplotlib.pyplot as plt
 from osgeo import gdal
 
 XMAXSIZE, YMAXSIZE = 2048, 2048
@@ -30,22 +31,25 @@ def open_raster(path, band=1, maxsize=False, crop=None):
     if crop is None:
         crop = [0, 0, band.XSize, band.YSize]
     if not maxsize:
-        array = band.ReadAsArray(crop[0], crop[1], crop[2], crop[3])
+        array = band.ReadAsArray(crop[0], crop[1], crop[2] - crop[0], crop[3] - crop[1])
     else:
-        array = band.ReadAsArray(crop[0], crop[1], crop[2], crop[3], XMAXSIZE, YMAXSIZE, resample_alg=gdal.GRIORA_Bilinear)
+        array = band.ReadAsArray(crop[0], crop[1], crop[2] - crop[0], crop[3] - crop[1], XMAXSIZE, YMAXSIZE, resample_alg=gdal.GRIORA_Bilinear)
     if ndv is not None and ndv != np.nan:
         array[array == ndv] = np.nan
     return array
 
 
-def copy_raster(raster, output):
+def copy_raster(raster, output,band=1):
     ds_raster = gdal.Open(raster)
     driver = gdal.GetDriverByName('GTiff')
-    driver.CreateCopy(output, ds_raster)
+    driver.CreateCopy(output, ds_raster, band)
 
 
-def median_raster(raster, crop=None, band=1):
+def median_raster(raster, crop=None, band=1, plot=False):
     r4med = open_raster(raster, band=band, maxsize=True, crop=crop)
+    if plot:
+        plt.imshow(r4med)
+        plt.show()
     return np.nanmedian(r4med.flatten())
 
 
@@ -55,7 +59,7 @@ def substract_median(median, output):
     ndv = band.GetNoDataValue()
 
     for y in range(band.YSize):
-        line = open_raster(output, crop=[0, y, band.XSize, 1])
+        line = open_raster(output, crop=[0, y, band.XSize, 1 + y])
         new_line = line - median
         new_line[new_line == np.nan] = ndv
         band.WriteArray(new_line, 0, y)
@@ -67,6 +71,7 @@ if __name__ == "__main__":
     output = arguments["<output>"]
     crop = arguments["--crop"]
     band = arguments["--band"]
+    plot = arguments["--plot"]
     if band is not None:
         band = int(band)
     else:
@@ -75,7 +80,7 @@ if __name__ == "__main__":
         crop = [int(c) for c in crop.split(",")]
 
     print("Compute median...")
-    median = median_raster(raster, crop=crop, band=band)
+    median = median_raster(raster, crop=crop, band=band, plot=plot)
     print("median =", median)
 
     print("Copy raster...")
